9ed6020dfa4b349f17fb495851064665e54be9c8
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-20 20:22:55 +0000

    User show page after log in is working now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    render json: @users
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
-  end
+      @user = User.find(params[:id])
+      p '****HERE****', @user
+      render json: @user
+    end
 end
