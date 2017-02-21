cb256a59aca3b5c7475f499061e54c878b965eeb
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-21 11:31:17 +0000

    all required user controllers have now been checked

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -24,7 +24,7 @@ class AuthenticationsController < ApplicationController
   private
     def user_params
       hash = {}
-      hash.merge! params.slice(:email, :password, :password_confirmation)
+      hash.merge! params.slice(:alias, :email, :password, :password_confirmation, :city, :sector)
       hash
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
-
+  put 'users/:id', to: 'users#update'
   resources :issues
   resources :messages
   post 'register', to: 'authentications#register'
