e2960024826fa20b00e67999ced1ac0a5fcef934
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-21 10:19:01 +0000

    Current user in HTML and Controllers has not been sorted out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
-
   def show
-      @user = User.find(params[:id])
-      render json: @user
-    end
+    user = User.find(params[:id])
+    render json: { user: UserSerializer.new(user) }
+  end
 end
