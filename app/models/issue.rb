9100d2fa913939d8940b2f4b8158d98458a310bf
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-22 22:28:14 +0000

    some more config changes for the emailer but still doesnt work

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,16 +2,16 @@ class Issue < ApplicationRecord
   belongs_to :user
   has_many :messages
 
-  before_save :send_emails
-
-  private
-  def send_emails
-    users = User.where(city: self.user.city)
-
-    emailsArray = []
-    for user in users
-      emailsArray.push(user.email)
-    end
-    EmailFriendsMailer.find_friends_email(emailsArray).deliver_now
-  end
+  # before_save :send_emails
+  #
+  # private
+  # def send_emails
+  #   users = User.where(city: self.user.city)
+  #
+  #   emailsArray = []
+  #   for user in users
+  #     emailsArray.push(user.email)
+  #   end
+  #   EmailFriendsMailer.find_friends_email(emailsArray).deliver_now
+  # end
 end
