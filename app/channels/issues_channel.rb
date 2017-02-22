beaad8fc64f47703089541cd455834bb74c573b1
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-22 18:35:45 +0000

    saving the Action Cable stuff

---
+++ app/channels/issues_channel.rb
@@ -0,0 +1,25 @@
+class IssuesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from stream_name
+  end
+
+  # generic
+  def receive(data)
+    ActionCable.server.broadcast stream_name, data.fetch('message')
+  end
+
+  def new_message(data)
+p "*************************************"
+    ActionCable.server.broadcast stream_name, data.fetch('message')
+  end
+
+  private
+
+    def stream_name
+      "issues_channel_#{issue_id}"
+    end
+
+    def issue_id
+      params.fetch('data').fetch('id')
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   get 'users/:id', to: 'users#show'
   put 'users/:id', to: 'users#update'
   resources :issues
   resources :messages
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
+
+  mount ActionCable.server => '/cable'
 end
