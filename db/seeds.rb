344b8c876e437dec26d916f6f7e0c2f55aa40045
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-22 16:31:00 +0000

    updated the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ user1 = User.create!(alias: "MissDavinderKaur", email:"davinder@kaur.com", passw
 
 user2 = User.create!(alias: "MissHarpreetKaur", email:"harpreet@kaur.com", password:"password", password_confirmation:"password", city:"London", sector:"Consulting Services")
 
-issue1 = user1.issues.create!(title:"My super serious sexism issue", category:"sexism")
+issue1 = user1.issues.create!(title:"My super serious sexism issue", category:"sexism", active: true)
 
 issue1.messages.create!(sender_id: user2.id, receiver_id: user1.id, msg_text:"Some advice")
 
 issue1.messages.create!(sender_id: user1.id, receiver_id: user1.id, msg_text:"Thank you for the advice")
 
-issue2 = user1.issues.create!(title:"Another super serious issue", category:"racism")
+issue2 = user1.issues.create!(title:"Another super serious issue", category:"racism", active: true)
 
 issue2.messages.create!(sender_id: user2.id, receiver_id: user1.id, msg_text:"Some good advice")
 
