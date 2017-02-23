b7f8282a675586a42109b2a4ca7adaeb5aaedc5e
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-23 10:41:09 +0000

    Decided to pass issue title thru to emails

--- app/mailers/email_friends_mailer.rb
+++ app/mailers/email_friends_mailer.rb
@@ -3,7 +3,8 @@ include SendGrid
 
 class EmailFriendsMailer < ApplicationMailer
 
-  def find_friends_email(emailsArray)
+  def find_friends_email(emailsArray, title)
+    @title = title
     @url  = 'http://localhost:7000/login'
     mail(to: emailsArray, subject: 'Let\'s Talk - There is a Friend In Need in your city!')
   end
